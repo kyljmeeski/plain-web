f434cc3692c5436fc816e7cb1a27abdb06f3a965
kyljmeeski <kyljmeeski@gmail.com>
2024-07-06 01:24:22 +0600

    doc: add javadocs to Route and Routes

--- src/main/java/com/kyljmeeski/plainweb/Route.java
+++ src/main/java/com/kyljmeeski/plainweb/Route.java
@@ -1,7 +1,16 @@
 package com.kyljmeeski.plainweb;
 
+/**
+ * Represents a route handler that processes incoming requests and returns a response.
+ */
 public interface Route {
 
+    /**
+     * Handles the incoming request and returns a response.
+     *
+     * @param request The incoming request to be processed
+     * @return The response generated based on the request
+     */
     Response handle(Request request);
 
 }
--- src/main/java/com/kyljmeeski/plainweb/Routes.java
+++ src/main/java/com/kyljmeeski/plainweb/Routes.java
@@ -7,12 +7,21 @@ import com.kyljmeeski.plainweb.response.NotFoundResponse;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Manages registered routes and handles incoming requests by matching them to registered routes.
+ */
 public class Routes {
 
     private final Map<String, Pair> routes = new HashMap<>();
     private final Response notFoundResponse = new NotFoundResponse();
     private final Response methodNotAllowedResponse = new MethodNotAllowedResponse();
 
+    /**
+     * Generates a response for the given request by matching it against registered routes.
+     *
+     * @param request The incoming request to be processed
+     * @return The generated response based on the request and registered routes
+     */
     public Response response(Request request) {
         Pair pair = routes.get(request.path());
         if (pair == null) {
@@ -27,10 +36,21 @@ public class Routes {
         return methodNotAllowedResponse;
     }
 
+    /**
+     * Adds a new route with the specified HTTP method and path.
+     *
+     * @param method The HTTP method of the route (GET, POST, PUT, etc.)
+     * @param path   The path pattern for the route
+     * @param route  The handler for the route
+     */
     public void addRoute(HttpMethod method, String path, Route route) {
         routes.put(path, new Pair(method, route));
     }
 
+
+    /**
+     * Private inner class to represent a pair of HTTP method and Route handler.
+     */
     private static class Pair {
 
         private final HttpMethod method;
