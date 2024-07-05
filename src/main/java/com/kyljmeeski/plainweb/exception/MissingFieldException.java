0ae3b0d0f3753db9ecd641b157c2655879c4f37e
kyljmeeski <kyljmeeski@gmail.com>
2024-07-06 01:19:58 +0600

    doc: add javadocs to MissingFieldException

--- src/main/java/com/kyljmeeski/plainweb/exception/MissingFieldException.java
+++ src/main/java/com/kyljmeeski/plainweb/exception/MissingFieldException.java
@@ -1,7 +1,18 @@
 package com.kyljmeeski.plainweb.exception;
 
+/**
+ * Exception thrown when a required field is missing or not provided.
+ */
 public class MissingFieldException extends Exception {
+
+    /**
+     * Constructs a MissingFieldException with the specified error message.
+     *
+     * @param message The error message describing the missing field issue
+     */
     public MissingFieldException(String message) {
         super(message);
     }
+
 }
+
